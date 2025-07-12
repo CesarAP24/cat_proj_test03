4a7aba2aa97475fae9d6d418ad4aa982bb64bd23
Jose <jose@MacBook-Air-de-Jose.local>
2025-07-12 00:28:15 -0500

    add Menu

---
+++ tests/menu.gd
@@ -0,0 +1,11 @@
+# Script adjunto al nodo raíz MenuGato
+extends Control
+
+# La ruta baja por el árbol: VBoxContain -> Jugar
+@onready var primer_boton = $Buscador/mandarSeed
+
+func _ready():
+	primer_boton.grab_focus()
+
+func _on_jugar_pressed():
+	get_tree().change_scene_to_file("res://tests/abandoned.tscn")
