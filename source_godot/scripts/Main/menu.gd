9f4453aa776a5535211c4e8b85d325b8ab959d88
Jose <jose@MacBook-Air-de-Jose.local>
2025-07-14 00:57:07 -0500

    te odio cache

--- source_godot/scripts/Main/menu.gd
+++ source_godot/scripts/Main/menu.gd
@@ -1,11 +1,17 @@
 # Script adjunto al nodo raíz MenuGato
 extends Control
 
-# La ruta baja por el árbol: VBoxContain -> Jugar
-@onready var primer_boton = $Buscador/mandarSeed
-
 func _ready():
-	primer_boton.grab_focus()
-
+	pass
+	
 func _on_jugar_pressed():
 	get_tree().change_scene_to_file("res://tests/city_test_night.tscn")
+
+func _on_great_pressed():
+	get_tree().change_scene_to_file("res://tests/thegreat.tscn")
+	
+func _on_egg_pressed():
+	get_tree().change_scene_to_file("res://tests/abandoned.tscn")
+
+func _on_test_pressed():
+	get_tree().change_scene_to_file("res://tests/test03.tscn")
